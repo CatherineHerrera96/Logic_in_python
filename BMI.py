fccdfc49f28fb911ad5e472045acaf2a8087d668
Catherine <kte.tefi@gmail.com>
2022-01-20 18:03:31 -0500

    english BMI

--- BMI.py
+++ BMI.py
@@ -7,8 +7,17 @@ ya que su función será usada en los Estados Unidos.
 Recuerde que:
 1 libra corresponde a 0.45kg.
 1 pulgada corresponde a 0.025 metros.
-
 El valor de retorno debe estar redondeado a dos decimales. 
+
+Create a function that can calculate a person's body mass index (BMI).
+The formula for calculating the BMI is as follows: 
+BMI = weight/(height^2)
+In this formula the weight is in kilograms and the height in meters. Keep in mind that the weight and height that receive their function, will be in pounds and inches respectively,
+since its function will be used in the United States.
+Remember that:
+1 pound corresponds to 0.45kg.
+1 inch corresponds to 0.025 meters.
+The return value must be rounded to two decimal places.
 
 @CatherineH
 """
