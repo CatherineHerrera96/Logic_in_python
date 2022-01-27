b6446979625c2150bc14ab486b1e31bba4b674f8
Catherine <kte.tefi@gmail.com>
2022-01-27 16:04:00 -0500

    convertidor de temperaturas

---
+++ conver_temperarura.py
@@ -0,0 +1,21 @@
+"""
+defina funciones para co grados en las diferentes escalas 
+
+
+@CatherineH
+"""
+def fahrenheit_a_centigrados (gradosF : float) -> float:   
+    return  (gradosF - 32)*(5/9)
+
+def centigrados_a_fahrenheit (gradosC : float) -> float:
+    return (gradosC*(9/5))/1.8
+
+gradosF = float(input("ingrese los grados fahrenheit que quiere convertir a centigrados: "))
+gradosC = float(input("ingrese los grados centigrados que quiere convertir a fahrenheit: "))
+
+
+CaF  = centigrados_a_fahrenheit(gradosC)
+print(f"{gradosC} grados centigrados son iguales a {round(CaF,1)} grados fahrenheit")
+
+FaC = fahrenheit_a_centigrados(gradosF)
+print(f"{gradosF} grados fahrenheit son iguales a {round(FaC,1)} grados centigrados")
