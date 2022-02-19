fddb27feafd0220232aa378dfcb91a06e1887721
Catherine <kte.tefi@gmail.com>
2022-02-19 10:24:05 -0500

    ordenar archivos

---
+++ operators/banco.py
@@ -0,0 +1,11 @@
+numero_pesos = float(input("introduzca numero de numero de pesos: "))
+tasa_interes = float(input("introduzca la tasa de intereses: "))
+numero_anios = int(input("introduzca numero de a�os: "))
+
+
+C = numero_pesos
+X = tasa_interes
+n = numero_anios
+
+valor_futuro = C * (1 + (X/100))**n
+print("el valor que se pagara en el futuro es de : " + str(round(valor_futuro)) + ", transcurridos " + str(n) + " a�os" + " " + "a una tasa anual del" + " " + str(X) + "%")
